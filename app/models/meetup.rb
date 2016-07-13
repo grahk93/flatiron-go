f28e7294ee9e37505c05ec31be2d3c1b5b1dbcbf
grahk93 <grace.juster@flatironschool.com>
2016-07-13 18:26:46 -0400

    added structure for user show page queries and methods

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -3,4 +3,30 @@ class Meetup < ApplicationRecord
   #belongs_to :location
   has_many :meetup_attendants
   has_many :attendants, through: :meetup_attendants
-end
+
+  def today
+  end
+
+  def this_week
+  end
+
+  def by_cohort
+  end
+
+  def by_location
+  end
+
+  def by_host
+  end
+
+  def pop_location
+  end
+
+  def pop_day
+  end
+
+  def pop_time
+  end 
+
+
+end 
