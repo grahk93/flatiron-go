76d506bb59b1fc69232923b22548bb4984819cad
grahk93 <grace.juster@flatironschool.com>
2016-07-13 10:00:25 -0400

    Add url helpers to routes and build out sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,21 @@
 class SessionsController < ApplicationController
-
+  skip_before_action :login_required, only: [:new, :create]
   def new 
-    @user = User.new
   end 
 
   def create
-    
-  end 
+    user = User.find_by(user_name: params[:user][:user_name])
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id 
+      redirect_to user_path(session[:user_id])
+    else
+      render 'new'
+    end
+  end
 
   def destroy
-
+    session[:user_id] = nil
+    redirect_to login_path
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
-  get '/login' => 'sessions#new'
+  get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  get '/users/new' => 'users#new' #signup 
+  get '/users/new' => 'users#new', as: 'signup'
   post '/users' => 'users#create'
-  get '/users/:id' => 'users#show'
-  get '/users/:id/edit' => 'users#edit'
+  get '/users/:id' => 'users#show', as: 'user'
+  get '/users/:id/edit' => 'users#edit', as: 'edit_user'
   patch '/users/:id' => 'users#update'
 
 
