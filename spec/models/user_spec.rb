99570cee950f3d6bf6504c131504bd66662133e0
chriswigington <chriswigington@gmail.com>
2016-07-16 13:57:05 -0400

    Fixed some small issues with existing rspec tests I had written

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do 
   let(:cohort) { FactoryGirl.create(:cohort) }
-
   let(:user) { FactoryGirl.create(:user, cohort_id: cohort.id) }
 
-  #basics
   it "is valid" do 
     expect(user).to be_valid
   end
@@ -14,9 +12,9 @@ RSpec.describe User, :type => :model do
     expect(user.cohort).to eq(cohort)
   end
 
-  #space for methods
+  let(:host) { Host.create(user: user)}
+
   it "can become a host" do
-    let(:host) { FactoryGirl.create(:host, user: user)}
     expect(host.user).to eq(user)
   end
 end
